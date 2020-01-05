87bf70653927a0ed223e87873387cc04994f20f1
Roland Szabó <roland@rolandszabo.com>
2020-01-05 19:33:40 +0100

    Implement rotation for ultrasonic control

--- api/camera_control_servicer.py
+++ api/camera_control_servicer.py
@@ -1,10 +1,9 @@
 import logging
-import numpy as np
 import generated.camera_control_pb2_grpc
 
-from generated.camera_control_pb2 import SetRotationResponse
+from generated.camera_control_pb2 import SetCameraRotationResponse
 from hardware.controller import Controller
-from utils.servo import angle_to_pwm
+from utils.servo import angle_to_pwm, clip_horizontal_angle, clip_vertical_angle
 
 logger = logging.getLogger(__name__)
 
@@ -18,12 +17,12 @@ class CameraControlServicer(generated.camera_control_pb2_grpc.CameraControlServi
         logger.info('Set camera rotation to horizontal angle {} and vertical angle {}'.format(request.horizontal,
                                                                                               request.vertical))
 
-        horizontal_angle = np.clip(request.horizontal, np.deg2rad(0), np.deg2rad(180))
-        logger.debug('Normalized horizontal angle is {}'.format(horizontal_angle))
+        horizontal_angle = clip_horizontal_angle(request.horizontal)
+        logger.debug('Clipped horizontal angle is {}'.format(horizontal_angle))
 
-        vertical_angle = np.clip(request.vertical, np.deg2rad(90), np.deg2rad(180))
-        logger.debug('Normalized vertical angle is {}'.format(vertical_angle))
+        vertical_angle = clip_vertical_angle(request.vertical)
+        logger.debug('Clipped vertical angle is {}'.format(vertical_angle))
 
         self.controller.write(Controller.CMD_SERVO2, angle_to_pwm(horizontal_angle))
         self.controller.write(Controller.CMD_SERVO3, angle_to_pwm(vertical_angle))
-        return SetRotationResponse()
+        return SetCameraRotationResponse()
--- api/ultrasonic_control_servicer.py
+++ api/ultrasonic_control_servicer.py
@@ -1,8 +1,9 @@
 import logging
 import generated.ultrasonic_control_pb2_grpc
 
-from generated.ultrasonic_control_pb2 import GetDistanceResponse
+from generated.ultrasonic_control_pb2 import GetDistanceResponse, SetUltrasonicRotationResponse
 from hardware.controller import Controller
+from utils.servo import clip_horizontal_angle, angle_to_pwm
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +12,15 @@ class UltrasonicControlServicer(generated.ultrasonic_control_pb2_grpc.Ultrasonic
 
     def __init__(self, controller):
         self.controller = controller
+
+    def SetRotation(self, request, context):
+        logger.info('Set ultrasonic rotation to horizontal angle {}'.format(request.horizontal))
+
+        horizontal_angle = clip_horizontal_angle(request.horizontal)
+        logger.debug('Clipped horizontal angle is {}'.format(horizontal_angle))
+
+        self.controller.write(Controller.CMD_SERVO2, angle_to_pwm(horizontal_angle))
+        return SetUltrasonicRotationResponse()
 
     def GetDistance(self, request, context):
         logger.info('Get distance')
